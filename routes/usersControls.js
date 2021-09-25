f31ce8a392059f51cb9d117b83e6536c3f423a1d
shuhaibzahir <yesshuhaib.ss@gmail.com>
2021-09-25 08:27:42 +0530

    user Sign in started

--- routes/usersControls.js
+++ routes/usersControls.js
@@ -19,5 +19,20 @@ const Signup=(req,res,next)=>{
         console.log(error)
     }
 }
+const SignIN=(req,res,next)=>{
+    const data = req.body
+    console.log(req.body)
+   try{
+       userHelper.userSignUp(data).then((userSignupData)=>{
+            console.log(userSignupData)
+           res.status(200).json({result:userSignupData})
+       }).catch((er)=>{
+           console.log(er)
+           res.status(401).json({apiError:er})
+       })
+   }catch (error){
+       console.log(error)
+   }
+}
 
 module.exports={Signup}
