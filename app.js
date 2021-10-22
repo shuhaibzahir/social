7d05dbe15114520bec21a674efb5999a659e26ca
shuhaibzahir <yesshuhaib.ss@gmail.com>
2021-10-22 19:30:44 +0530

    this is removed the headers

--- app.js
+++ app.js
@@ -25,14 +25,7 @@ var app = express();
 
 
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-  });
-  
+
 
 app.use(cors({
   origin: "http://localhost:3000"
